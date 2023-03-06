c38b49ef101545a6ef3e59bf325eddc33494c06e
Ljason <qjdlsp33@naver.com>
2023-03-06 22:28:21 +0900

    response

---
+++ js/index.js
@@ -0,0 +1,267 @@
+
+
+import $ from 'jquery'
+ 
+$(function(){
+
+let windowW=$(window).width()
+// 웹
+
+
+if(1545<=windowW){
+    nav()
+    submenu()
+    a()
+}
+// 웹에서 태블릿으로 가는 과정
+else if(980 <= windowW && windowW <1544){
+    nav()
+    submenu()
+    a()
+}
+
+// 태블릿
+// box04 문제 때문에 979가 아니라 973으로 수정함
+else if(580 <=windowW && windowW<973){
+    tNav()
+    gallery()
+    a()
+}
+
+// 모바일
+else if(windowW<=579){
+    tNav()
+    gallery()
+    a()
+}
+
+// 공통
+// reset : 포트폴리오
+
+$(window).on('resize',function(e){
+window.location.reload();
+})
+
+
+//
+})
+
+//  web nav
+function nav(){
+
+$('nav li>a').on('click',function(e){
+    const navA=$(this).attr('href');
+    const aPos= $(navA).offset().top ;
+    const headerHeight =$('header').innerHeight();
+    $('html,body').animate({scrollTop:aPos - headerHeight},800);
+    return false;
+})
+}
+// table,mobil nav
+function tNav(){
+
+    let navW=$('nav').width()
+
+        // .btn click
+    $('header .btn').on('click',function(e){
+        $('nav').animate({left:0},500)
+        $(this).hide()
+
+    })
+
+
+
+    // scroll
+
+    $('nav li>a').on('click',function(e){
+        const navA=$(this).attr('href');
+        const aPos= $(navA).offset().top ;
+        const headerHeight =$('header').innerHeight();
+        $('html,body').animate({scrollTop:aPos - headerHeight},800);
+        $('nav').css('left','-'+navW+'px')
+        $('header .btn').show();
+        return false;
+    })
+
+
+    // close
+
+    $('nav .close').on('click',function(e){
+        $('nav').css('left','-'+navW+'px')
+        $('header .btn').show();
+    })
+}
+
+
+function submenu(){
+// html 연결
+//jquery 
+$('aside li>a').on('click',function(e){
+const asideA=$(this).attr('href');
+const asidePos=$(asideA).offset().top;
+const headerHeight=$('header').innerHeight();
+$('html,body').animate({scrollTop:asidePos- headerHeight},800);
+return false;
+})
+}
+
+
+
+function gallery(){
+
+// 준비하기
+const figureW=$('#box04 #all figure').width();
+$('#all figure:last').prependTo('#all')
+$('#all').css('margin-left','-'+figureW+'px')
+
+
+// 이벤트
+$('#gallery .prev').on('click',function(e){
+    $('#all').animate({marginLeft:'-='+figureW+'px'},400,function(){
+        $('#all figure:first').appendTo('#all')
+        $('#all').css('margin-left','-'+figureW+'px')    
+    })
+    })
+    
+    $('#gallery .next').on('click',function(e){
+    $('#all').animate({marginLeft:'+='+figureW+'px'},400,function(){
+        $('#all>figure:last').prependTo('#all')
+        $('#all').css('margin-left','-'+figureW+'px')
+    })
+    })
+}
+
+
+
+
+
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// modal 클릭하면 화면 나오게 하기.
+
+const h4=document.querySelector('#modal h4');
+const img=document.querySelector('#modal figure>img');
+const day=document.querySelector('#modal dl .year');
+const pro=document.querySelector('#modal dl .program');
+const url=document.querySelector('#modal dl .link>a');
+const content=document.querySelector('#modal dl .text');
+const fig=document.querySelector('#modal figcaption')
+
+const open=document.querySelectorAll('#all>figure')
+// console.log(open)
+const close=document.querySelector('#modal>.close')
+// console.log(close)
+const modal=document.querySelector('#modal')
+
+
+
+// 객체를 생성자 함수로 만들 것
+function Modal(title,pic,year,program,href,text,fig){
+    this.title=title;
+    this.pic=pic;
+    this.year=year;
+    this.program=program;
+    this.href=href;
+    this.text=text;
+    this.fig=fig;
+}
+
+
+
+
+
+
+// 매서드
+
+// 객체안에 있는 정보를 수정하는 행위
+Modal.prototype.action=function(){
+h4.innerHTML=this.title;
+img.setAttribute('src',this.pic);
+day.innerHTML=this.year;
+pro.innerHTML=this.program;
+url.setAttribute('href',this.href);
+url.innerHTML=this.href;
+content.innerHTML=this.text;
+fig.innerHTML=this.fig;
+}
+
+
+
+// 인스턴스
+let myModal=[
+new Modal('인생막걸리','./images/design.png','2022','html, css','https://jason-picture01-1223.s3.ap-northeast-2.amazonaws.com/text01/index.html',' 서울탁주에서 만든 인생막걸리와 달빛유자막걸이에 대한 홍보및 자세한 설명이 담겨있는 사이트 입니다.','작업물01'),
+new Modal('REEBOK','./images/design2.png','2022','html, css','https://jason-picture01-1223.s3.ap-northeast-2.amazonaws.com/test02/index.html','리복 공식 온라인스토어에서 판매하는 신발에 대한 홍보와 설명이 담겨져 있는 사이트입니다.','작업물02'),
+new Modal('SOHOME','./images/design3.png','2022','html, css','https://jason-picture01-1223.s3.ap-northeast-2.amazonaws.com/test03/index.html','다양한 가구들을 판매하는 주식회사 쏘홈입니다.','작업물03'),
+new Modal('title04','./images/design4.png','2022','html, css','http://aaa4.com','내용4','작업물04'),
+new Modal('title05','./images/design5.jpg','2022','html, css','http://aaa5.com','내용5','작업물05'),
+new Modal('title06','./images/design6.png','2022','html, css','http://aaa6.com','내용6','작업물06')
+]
+
+
+
+
+
+
+// event->작업 ->click(figure,figure>img,#modal>.close)
+
+
+// 여러개를 클릭해야되니까 forEach를 써야함.
+open.forEach(function(item,index){
+item.onclick=function(){
+    modal.style.display='block'
+    myModal[index].action();
+}
+})
+
+
+// 한개만 클릭해도 되니까 forEach 안씀.
+close.onclick=function(){
+    modal.style.display='none';
+}
+
+
+
+/////////////////////////box04 ul hover하면 border 나오게하기///////////////////////////
+
+
+
+function a(){
+
+
+$('.html>a').hover(function(){
+    $('#all .article1').css('border','1px solid black')
+    $('#all .article2').css('border','1px solid black')
+    $('#all .article3').css('border','1px solid black')
+
+},function(){
+    $('#all>figure').css('border','none')
+
+})
+
+$('.javascript>a').hover(function(){
+    $('#all .article4').css('border','1px solid black')
+
+},function(){
+    $('#all>figure').css('border','none')
+
+})
+
+
+$('.jquery>a').hover(function(){
+    $('#all .article5').css('border','1px solid black')
+
+},function(){
+    $('#all>figure').css('border','none')
+
+})
+
+
+$('.react>a').hover(function(){
+    $('#all .article6').css('border','1px solid black')
+
+},function(){
+    $('#all>figure').css('border','none')
+
+})
+
+}
